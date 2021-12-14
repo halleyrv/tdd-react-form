260e5163f16d4f4268c3bf1f69bb5614cdeb7e71
Halle <francrv@hotmail.com>
2021-12-13 23:22:17 -0500

    chore: create form component with initial test

---
+++ src/form/form.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Form = () => <h1>create Product</h1>;
+
+export default Form;
---
+++ src/form/form.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {screen, render} from '@testing-library/react';
+import {Form} from './form';
+
+describe('when the form is mounted', () => {
+  it('there must be a create product from page', () => {
+    render(<Form />);
+    expect(
+      screen.getByRole('heading', {name: /create product/i}),
+    ).toBeInTheDocument();
+  });
+});
